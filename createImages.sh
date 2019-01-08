65bee472ed98786a5206e41da4d61b50888bf772
Arsenii Venherak <indeooars@gmail.com>
2019-01-08 15:27:57 +0200

    fixed folder name in createImages.sh

--- createImages.sh
+++ createImages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -t telegram-opencart-api telegram-opencart/.
+docker build -t telegram-opencart-api telegram-opencart-api/.
 docker build -t telegram-opencart-bot telegram-opencart-bot/.
 
